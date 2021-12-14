fb5d63ee404f4f53dff97bf8997a05d03d95c873
edvinno <edvinno@kth.se>
2021-12-14 01:37:50 +0100

    Fixed bug where snake collides with previous tail

--- src/time4snake/mipslabwork.c
+++ src/time4snake/mipslabwork.c
@@ -240,11 +240,13 @@ int hitsWall() {
 }
 
 /*
-Jocke.
-If the position of the head is greater than 0, it has collided with itself.
+Jocke and Edvin.
+If the position of the head is greater than 0, it has collided with itself,
+unless it's the tail. (The tail will move next frame so the coordinate is up for grabs in that case.)
 */
 int snakeCollidedWithItself() {
-  if (snakeArray[yPos][xPos] > 0) { 
+
+  if (snakeArray[yPos][xPos] > 0 & snakeArray[yPos][xPos] != tail) { // A snake bodypart is at these coordinates, but not the tail.
     return 1;
   } else {
     return 0;
